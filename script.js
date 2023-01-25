cf215047d17973c87b14eecb0a9bc9d0f5ba364b
socry <leonardosbo02@gmail.com>
2023-01-25 14:31:16 -0300

    Add check functions and dot listener

--- script.js
+++ script.js
@@ -13,3 +13,76 @@ const divide = (x,y) =>{
     }
 }
 
+const operate = (operator,x,y) =>{
+    return operator == 'add'? add(x,y):
+    operator == 'subtract'? subtract(x,y):
+    operator == 'multiply'? multiply(x,y):
+    divide(x,y)
+}
+
+
+// Global Variables
+let displayValue = document.querySelector('#display p').innerHTML;
+let numbers = [];
+let multipleOperation = false;
+let currentOperator = null;
+let currentOperatorSymbol = null;
+let updatedOperatorSymbol = null;
+
+// Look for operators on calculator display
+const searchForOperators = () =>{
+
+    for (let i = 0; i <= document.querySelector('#display p').innerHTML.length - 1; i++){
+        if (document.querySelector('#display p').innerHTML[i] == '+' || document.querySelector('#display p').innerHTML[i] == '-' || document.querySelector('#display p').innerHTML[i] == '*' || document.querySelector('#display p').innerHTML[i] == '/'){
+            return true;
+        }
+    }
+    return false;
+}
+
+// Look for '.' on display value
+const searchForDot = () =>{
+    let count = 0;
+
+    for (let i = 0; i <= document.querySelector('#display p').innerHTML.length - 1; i++){
+
+        if (document.querySelector('#display p').innerHTML[i] == '.'){
+            count++;
+        }
+    }
+    return count;
+}
+
+clearButton = document.querySelector('#clear')
+
+// If user tries to divide something by zero
+const checkDivideBy0 = ()=>{
+    if (displayValue == 'undefined >.<'){
+        // reset if user tries to do math with the message
+        clearButton.dispatchEvent(new Event("click"));
+    }
+}
+
+// Dot ('.') EVENT LISTENER
+const dotButton = document.querySelector('#dot')
+
+dotButton.addEventListener('click', () =>{
+
+    // check for message outputted when user divides something by 0
+    checkDivideBy0();
+
+    // if user's inputting after an operator (5 / ''<-- here) then reset dot condition
+    if(displayValue.split(`${currentOperatorSymbol}`)[1] != undefined && displayValue.split(`${currentOperatorSymbol}`)[1] != ''){
+        if(searchForDot() <= 1){
+            displayValue += '.'
+            document.querySelector('#display p').innerHTML = displayValue;
+        }
+    }
+    // if user is inputting before operator
+    else{
+        if(searchForDot() == 0){
+            displayValue += '.'
+            document.querySelector('#display p').innerHTML = displayValue;
+        }
+    }
+})
